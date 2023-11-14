93d3c39c81c080fb978a2c29bdda51a8ff705c3c
Surendra Jammishetti <suri312006@gmail.com>
2023-11-14 00:37:04 -0800

    boom!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>This is a basic typescript vercel sveltekit scaffold!</p>
